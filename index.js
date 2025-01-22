b11ff52e1b7348d751dfcc54eda75bd3e476690d
Guest2 <guest2@Yogeshs-MacBook-Air.local>
2025-01-22 15:24:23 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require("express");
-const app = express();
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
-});
+const mongoose = require('mongoose')
+const cors = require('cors')
+const UserModel = require('./models/userDetails')
+const dotEnv=require('dotenv')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+dotEnv.config()
+
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    UserModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json('success')
+                }
+                else {
+                    res.json('password is incorrect')
+                }
+            } else {
+                 res.json('no record existed')
+            }
+        })
+})
+
+
+app.post('/register', (req, res) => {
+    UserModel.create(req.body)
+        .then(employees => res.json(employees))
+        .catch(err => res.json(err))
+})
 
 app.listen(5000, () => {
     console.log("Running on port 5000.");
